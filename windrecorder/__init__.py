adcefe8945c0995d2c57bdea782351dfed843845
harutono <antonoko@outlook.com>
2023-11-26 20:16:42 +0800

    Update __init__.py

--- windrecorder/__init__.py
+++ windrecorder/__init__.py
@@ -1,5 +1,5 @@
 import datetime
 
 # PEP 440-compliant version number
-__version__ = "0.0.4b2"
+__version__ = "0.0.4b3"
 UPDATE_DATETIME = datetime.datetime(2023, 11, 24, 23, 26, tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
