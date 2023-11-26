854aec2d72a1a1000d0e09118c2e626ab61f5d98
harutono <antonoko@outlook.com>
2023-11-26 19:40:51 +0800

    add i18n and tray icon

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import pystray
 from PIL import Image, ImageDraw
 
+from windrecorder.utils import get_text as _t
 
-def create_image(width, height, color1, color2):
+
+def create_pattern_image(width, height, color1, color2):
     # Generate an image and draw a pattern
     image = Image.new("RGB", (width, height), color1)
     dc = ImageDraw.Draw(image)
     dc.rectangle((width // 2, 0, width, height // 2), fill=color2)
     dc.rectangle((0, height // 2, width // 2, height), fill=color2)
+    return image
 
+
+def get_tray_icon():
+    image = Image.open("__assets__\\icon-tray.png")
+    image = image.convert("RGBA")
     return image
 
 
@@ -42,23 +49,35 @@ def openWebui(icon: pystray.Icon, item: pystray.MenuItem):
 
 def setup(icon: pystray.Icon):
     icon.visible = True
-    icon.notify("I'm up")
+    icon.notify(message=_t("tray_notify_text"), title=_t("tray_notify_title"))
 
 
 def main():
     # In order for the icon to be displayed, you must provide an icon
     pystray.Icon(
         "Windrecorder",
-        create_image(64, 64, "black", "white"),
+        get_tray_icon(),
         menu=pystray.Menu(
-            pystray.MenuItem(lambda item: "close webui" if webui_running else "open webui", openCloseWebui),
+            pystray.MenuItem(lambda item: _t("tray_webui_exit") if webui_running else _t("tray_webui_start"), openCloseWebui),
             pystray.MenuItem(
-                "goto localhost:8901", openWebui, visible=lambda item: webui_running, enabled=False, default=True
+                _t("tray_webui_address").format(address_port="地址+端口占位符"),
+                openWebui,
+                visible=lambda item: webui_running,
+                enabled=False,
+                default=True,
             ),
-            pystray.MenuItem(lambda item: "stop recording" if recording_running else "start recording", startStopRecording),
+            pystray.MenuItem(
+                lambda item: _t("tray_record_stop") if recording_running else _t("tray_record_start"), startStopRecording
+            ),
             pystray.Menu.SEPARATOR,
-            pystray.MenuItem(lambda item: "update" if checkUpdate() else "latest", update, enabled=lambda item: checkUpdate()),
-            pystray.MenuItem("exit", lambda icon, item: icon.stop()),
+            pystray.MenuItem(
+                lambda item: _t("tray_update_cta").format(version="版本信息占位符")
+                if checkUpdate()
+                else _t("tray_version_info").format(version="版本信息占位符"),
+                update,
+                enabled=lambda item: checkUpdate(),
+            ),
+            pystray.MenuItem(_t("tray_exit"), lambda icon, item: icon.stop()),
         ),
     ).run(setup=setup)
 
