e0e40e2cc2c14b844475754bab93382f1e6560ae
harutono <Antonoko@outlook.com>
2023-12-01 13:02:01 +0800

    fix: start up record fail (#51)
    * fix: start up record fail
    
    * tweak cd dir command

--- start_record.bat
+++ start_record.bat
@@ -1,9 +1,11 @@
 @echo off
 title Windrecorder - Recording Screening      == Windrecorder == Windrecorder == Windrecorder ==
 
+cd /d %~dp0
 for /F "usebackq delims=" %%A in (`python -m poetry env info --path`) do call %%A\Scripts\activate.bat
 
 color 60
 echo Starting Recording Screen...
+
 python "%~dp0\recordScreen.py"
 pause
