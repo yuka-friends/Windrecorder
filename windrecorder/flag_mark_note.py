bcba3a4628835e02c674e9252361f84cb1e59a38
harutono <Antonoko@outlook.com>
2023-12-15 20:56:43 +0800

    [WIP] add flag mark not window

--- windrecorder/flag_mark_note.py
+++ windrecorder/flag_mark_note.py
@@ -3,6 +3,7 @@ import os
 
 import pandas as pd
 import pyautogui
+import customtkinter
 
 import windrecorder.file_utils as file_utils
 import windrecorder.utils as utils
@@ -41,5 +42,50 @@ def add_new_flag_record_from_tray():
 def update_record():
     pass
 
+
+class Flag_mark_window(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Windrecorder")
+        dpi = self.winfo_fpixels('1i')
+        SCALE_FACTOR = dpi/72
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        window_width = 400
+        window_height = 190
+        target_x = int((screen_width *4/5) * SCALE_FACTOR)
+        target_y = int((screen_height *4/5) * SCALE_FACTOR)
+
+        self.geometry("%dx%d+%d+%d" % (window_width, window_height, target_x, target_y))
+        self.grid_columnconfigure((0,1), weight=1)
+        # self.attributes("-toolwindow", True)   # 移除放大与缩小选项
+        self.resizable(False, False)
+        self.iconbitmap('__assets__\\icon-tray.ico')
+        FONT_CONFIG = ("Microsoft YaHei", 13)
+        
+        customtkinter.set_appearance_mode("system")
+
+        self.label_added = customtkinter.CTkLabel(self, text="✔ 已添加此时的标记。", fg_color="transparent", font=FONT_CONFIG)
+        self.label_added.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+        self.label_time = customtkinter.CTkLabel(self, text="2023/09/08  17:34:26", fg_color="transparent", text_color="#878787", font=FONT_CONFIG)
+        self.label_time.grid(row=0, column=3, padx=10, pady=0, sticky="e")
+
+        self.textbox = customtkinter.CTkTextbox(master=self, height= 80, font=FONT_CONFIG)
+        self.textbox.grid(row=2, column=0, columnspan=4, padx=5, pady=5, sticky="ew")
+        self.textbox.insert("0.0", "备注：")
+
+        self.button_remove = customtkinter.CTkButton(self, text="❌ 移除标记", command=self.button_callback, width=0, fg_color=("#DFDDDD", "#3D3D3D"), text_color=("#3A3A3A", "#E7E7E7"), hover_color=("#d4cdc8","#303030"), font=FONT_CONFIG)
+        self.button_remove.grid(row=3, column=0, padx=5, pady=5, columnspan=1, sticky="ew")
+
+        self.button_add_note = customtkinter.CTkButton(self, text="✔ 添加备注", command=self.button_callback, fg_color=("#E5DBFB", "#8262c9"), text_color=("#4B357E", "#ffffff"), hover_color=("#cfbef6","#6f53af"), font=FONT_CONFIG)
+        self.button_add_note.grid(row=3, column=1, padx=5, pady=5, columnspan=3, sticky="ew")
+
+        
+    def button_callback(self):
+        print("button pressed")
+
+# app = Flag_mark_window()
+# app.mainloop()
 
 check_and_create_csv_if_not_exist()
