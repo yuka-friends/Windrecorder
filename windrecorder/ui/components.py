2da6366a803f5f599ba313a1e13c958debb261a0
harutono <Antonoko@outlook.com>
2024-02-16 16:51:18 +0800

    add search history record

--- windrecorder/ui/components.py
+++ windrecorder/ui/components.py
@@ -1,10 +1,17 @@
+import datetime
+import os
 from pathlib import Path
 
+import pandas as pd
 import streamlit as st
 
+from windrecorder import file_utils
 from windrecorder.config import config
 from windrecorder.db_manager import db_manager
+from windrecorder.logger import get_logger
 from windrecorder.utils import get_text as _t
+
+logger = get_logger(__name__)
 
 
 # 检测并渲染onboarding提示
@@ -41,3 +48,25 @@ def video_dataframe(df, heightIn=800):
         },
         height=heightIn,
     )
+
+
+def record_search_history(search_content, search_type, search_datetime=datetime.datetime.now()):
+    """记录搜索历史"""
+    # try:
+    if config.enable_search_history_record:
+        CSV_TEMPLATE_HISTORY = pd.DataFrame(columns=["search_content", "search_type", "search_datetime"])
+        if not os.path.exists(config.search_history_note_filepath):
+            file_utils.ensure_dir(config.userdata_dir)
+            file_utils.save_dataframe_to_path(CSV_TEMPLATE_HISTORY, file_path=config.search_history_note_filepath)
+
+        df = file_utils.read_dataframe_from_path(file_path=config.search_history_note_filepath)
+
+        new_data = {
+            "search_content": search_content,
+            "search_type": search_type,
+            "search_datetime": datetime.datetime.strftime(search_datetime, "%Y-%m-%d %H:%M:%S"),
+        }
+        df.loc[len(df)] = new_data
+        file_utils.save_dataframe_to_path(df, file_path=config.search_history_note_filepath)
+    # except Exception as e:
+    #     logger.error(e)
