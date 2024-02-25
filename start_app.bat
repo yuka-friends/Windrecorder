00a6cd1c3c8cc1efb56a6e0310a63a10826cb8f8
antonoko <antonoko@outlook.com>
2024-02-25 10:24:15 +0800

    Update start_app.bat

--- start_app.bat
+++ start_app.bat
@@ -5,6 +5,8 @@ color 75
 echo.
 echo   Initializing Windrecorder, please stand by...
 echo.
+echo   Please do not leave this window until it disappears
+echo.
 
 cd /d %~dp0
 for /F "tokens=* USEBACKQ" %%A in (`python -m poetry env info --path`) do call %%A\Scripts\activate.bat
