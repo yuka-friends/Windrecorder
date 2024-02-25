061295ab5f2c8add374b25db66f0785edfa765ab
antonoko <antonoko@outlook.com>
2024-02-25 13:18:32 +0800

    Update install_update.bat

--- install_update.bat
+++ install_update.bat
@@ -13,7 +13,6 @@ python -m poetry install
 
 for /F "usebackq tokens=*" %%A in (`python -m poetry env info --path`) do call %%A\Scripts\activate.bat
 
-cls
 color 0e
 title Windrecorder - Quick Setup
 python "%~dp0\onboard_setting.py"
